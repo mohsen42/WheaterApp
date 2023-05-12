cd26c71bd2202bdc3fc6e8c171308e69c676fd1d
mohsen <mohsen2latshah@gmail.com>
2023-05-12 23:23:52 +0430

    First Request T wheaterApi And Get Data

---
+++ src/main/java/com/dolatshah/weatherforcast/WeatherForcastApplication.java
@@ -0,0 +1,61 @@
+package com.dolatshah.weatherforcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@SpringBootApplication
+public class WeatherForcastApplication {
+    static String apiKey = "YourApiKey";
+    static String city = "Berlin";
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherForcastApplication.class, args);
+
+        try {
+            // Create the API URL with the city and API key
+            String apiUrl = "http://api.weatherapi.com/v1/current.json?key="+apiKey+"&q="+city+"&aqi=no";
+
+            // Create a URL object with the API URL
+            URL url = new URL(apiUrl);
+
+            // Open a connection to the URL
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            // Set the request method to GET
+            connection.setRequestMethod("GET");
+
+            // Get the response code
+            int responseCode = connection.getResponseCode();
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                // Read the response from the API
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line;
+                StringBuilder response = new StringBuilder();
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                reader.close();
+
+                // Print the response
+                System.out.println(response.toString());
+            } else {
+                System.out.println("Error: " + responseCode);
+            }
+
+            // Close the connection
+            connection.disconnect();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ src/test/java/com/dolatshah/weatherforcast/WeatherForcastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dolatshah.weatherforcast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherForcastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
