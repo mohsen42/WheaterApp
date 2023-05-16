5c6a265cdae130de878bc4b1b7b1c661ef3a884c
mohsen <mohsen2latshah@gmail.com>
2023-05-16 11:25:36 +0430

    add forcast ablitlity to application

--- src/main/java/com/dolatshah/weatherforcast/WeatherForcastApplication.java
+++ src/main/java/com/dolatshah/weatherforcast/WeatherForcastApplication.java
@@ -15,6 +15,7 @@ public class WeatherForcastApplication {
 
     static String city = "Berlin"; // your city you want to get Data
 
+    static String forcastUrl="http://api.weatherapi.com/v1/forecast.json?key=ffc660abc3aa4a61b74182652231205&q=London&days=1&aqi=no&alerts=no";
     public static void main(String[] args) {
         SpringApplication.run(WeatherForcastApplication.class, args);
 
@@ -56,7 +57,47 @@ public class WeatherForcastApplication {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        forcast();
+    }
+    public static void forcast(){
+        try {
+            // Create the API URL with the city and API key
 
+
+            // Create a URL object with the API URL
+            URL url = new URL(forcastUrl);
+
+            // Open a connection to the URL
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            // Set the request method to GET
+            connection.setRequestMethod("GET");
+
+            // Get the response code
+            int responseCode = connection.getResponseCode();
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                // Read the response from the API
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line;
+                StringBuilder response = new StringBuilder();
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                reader.close();
+
+                // Print the response
+                System.out.println(response.toString());
+            } else {
+                System.out.println("Error: " + responseCode);
+            }
+
+            // Close the connection
+            connection.disconnect();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
